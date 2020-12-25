c54fc1e5472a8f3fb4ddc1fde9730406ed492d63
Davi Melo <extremexgamer32@gmail.com>
2020-12-25 12:13:55 -0300

    feat: add toast notification styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
+import ToastContainer from './components/ToastContainer';
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
